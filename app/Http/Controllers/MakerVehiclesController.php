9bce1a8a5051804283dbf39d7c1909838056ea73
d.campanelli <d.campanelli@gmail.com>
2015-11-16 14:42:32 +0000

    Implementing store method for Vehicles

--- app/Http/Controllers/MakerVehiclesController.php
+++ app/Http/Controllers/MakerVehiclesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Maker;
 use Illuminate\Http\Request;
-
+use App\Http\Requests\CreateVehicleRequest;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -43,9 +43,19 @@ class MakerVehiclesController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateVehicleRequest $request, $makerId)
     {
-        //
+        $maker = Maker::find($makerId);
+
+        if(!$maker)
+        {
+            return response()->json(['message' => 'This maker does not exist', 'code' => 404], 404);
+        }
+        $values = $request->all();
+        $maker->vehicles()->create($values);
+
+        return response()->json(['message' => 'The vehicle associated was created'], 201);
+
     }
 
     /**
