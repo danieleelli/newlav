9ae3f935e6dd7a4daaf79f0af8c75e00e3a60aea
d.campanelli <d.campanelli@gmail.com>
2015-11-16 08:50:18 +0000

    Fifth commit (test)

--- app/User.php
+++ app/User.php
@@ -21,7 +21,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var string
      */
-    protected $table = 'users';s
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
